d9baf169cac9b18c0d7ef88fd929d456a139ebab
AbdujalolovaE'zoza <karimovaezoza2006@gmail.com>
2025-10-17 11:46:12 +0500

    biroz funksiya qushildi

--- js/app.js
+++ js/app.js
@@ -3,11 +3,48 @@ const formEdit = document.getElementById('form-edit')
 const listGroupTodo = document.getElementById('list-group-todo')
 // const messageCreate = document.getElementById('message-create')
 const time = document.getElementById('time')
-const modal = document.getElementById('modal')
-const overlay = document.getElementById('overlay')
-/* time elements */
+
+// time element
 const fullDay = document.getElementById('full-day')
 const hourEl = document.getElementById('hour')
 const minuteEl = document.getElementById('minute')
 const secondEl = document.getElementById('second')
-const closeEl = document.getElementById('close')
+
+let todos = JSON.parse(localStorage.getItem('')) ? JSON.parse
+(localStorage.getItem('list')) : []
+
+//setTods to Localstorage
+function setTodos(){
+    localStorage.setItem('list', JSON.stringify(todos))
+}
+
+
+// show todos
+function showTodos(){
+    const todos = JSON.parse(localStorage.getItem('list'))
+}
+
+// show error
+function showMessage(where, message){
+    document.getElementById(`${where}`).textContent = message
+
+
+    setTimeout(() =>{
+        document.getElementById(`${where}`).textContent = ''
+    } , 2500)
+}
+
+
+// get Todos
+formCreate.addEventListener('submit',(e)=> {
+ e.preventDefault()
+ const todoText = formCreate[ 'input-create'].value.trim()
+ formCreate.reset()
+ if(todoText.length){
+    todos.push({ text:todoText, time:'10:03, 17.10.2025', completed:
+        false})
+        setTodos()
+ }else{
+    showMessage('message-create', 'Please, Enter some todo...')
+ }
+})
